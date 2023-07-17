6c07c820c8c2ace994fe54bade85a4da70c5eb99
Illia Kozlov <kozlovilla000@gmail.com>
2023-07-17 17:16:07 +0300

    add styles and bgc for the most hazardous neos

--- src/components/NeosList/NeosList.jsx
+++ src/components/NeosList/NeosList.jsx
@@ -1,4 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import './NeosList.scss';
+import classNames from "classnames";
+
 import React, { useEffect, useState } from "react";
 import { getNeos } from "../../services/neo";
 import { Loader } from "../Loader";
@@ -54,6 +57,15 @@ export const NeosList = () => {
   }, [currentDay]);
   
 
+  const getHighestHazardousCounts = () => {
+    const sortedNeos = [...neos].sort(
+      (a, b) => b.hazardousCount - a.hazardousCount
+    );
+    return sortedNeos.slice(0, 2);
+  };
+
+  const highestHazardousCounts = getHighestHazardousCounts();
+
   return (
     <div>
       <h1>Near Orbital Objects (NEO)</h1>
@@ -71,17 +83,21 @@ export const NeosList = () => {
           }) => (
             <li 
               key={Math.random()}
-              className="card"
+              className={classNames("card", {
+                "card--red": highestHazardousCounts.some(
+                  (neo) => neo.hazardousCount === hazardousCount
+                ),
+              })}
             >
-              <h2 className="card__Item">
+              <h2 className="card__item">
                 Date: {date}</h2>
-              <p className="card__Item">
+              <p className="card__item">
                 Max Estimated Diameter (km): {maxDiameter}</p>
-              <p className="card__Item">
+              <p className="card__item">
                 Potentially Hazardous NEOs: {hazardousCount}</p>
-              <p className="card__Item">
+              <p className="card__item">
                 Closest NEO (km): {closestNeo}</p>
-              <p className="card__Item">
+              <p className="card__item">
                 Fastest NEO (kph): {fastestNeo}</p>
             </li>
           ))
